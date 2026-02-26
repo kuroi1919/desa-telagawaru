71c92f3e35619701da0069fedda7f61aee52d549
Syahril Ramdani <syahrilramdani@MacBook-Pro-Syahril.local>
2026-02-27 00:05:45 +0800

    fix dashboard export

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+export default function DashboardPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">
+        Halaman Dashboard
+      </h1>
+    </div>
+  )
+}
