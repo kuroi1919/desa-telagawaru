5adfd50692425f34304974fb4f0ccd21e3ac5a7a
Syahril Ramdani <syahrilramdani@MacBook-Pro-Syahril.local>
2026-02-26 23:57:08 +0800

    initial clean setup

---
+++ app/permohonan/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+export default function PermohonanPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">
+        Form Pengajuan Surat
+      </h1>
+    </div>
+  )
+}
